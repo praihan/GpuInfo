d9e7905b7bbb5ba7934824e268ff4f9f9dc17924
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-10-17 19:10:00 -0400

    Use anonymous namespace for nvidia_device

--- src/GpuInfo.cc
+++ src/GpuInfo.cc
@@ -10,33 +10,34 @@
 
 namespace gpuinfo {
 
-  struct nvidia_device : device {
-    nvidia_device(NvPhysicalGpuHandle gpu_handle) : _gpu_handle(gpu_handle) {
-      // TODO: handle error
-      assert(this->_gpu_handle != nullptr);
-    }
-
-    virtual std::string name() const override {
-      NvAPI_ShortString name_string;
-      NvAPI_Status status = NVAPI_OK;
-
-      status = NvAPI_GPU_GetFullName(this->_gpu_handle, name_string);
-      if (status != NVAPI_OK) {
+  namespace {
+    struct nvidia_device : device {
+      nvidia_device(NvPhysicalGpuHandle gpu_handle) : _gpu_handle(gpu_handle) {
         // TODO: handle error
+        assert(this->_gpu_handle != nullptr);
       }
 
-      return std::string{ name_string };
-    }
+      virtual std::string name() const override {
+        NvAPI_ShortString name_string;
+        NvAPI_Status status = NVAPI_OK;
 
-    virtual memory_info memory() const override {
-      return memory_info();
-    }
-    virtual std::vector<thermal_sensor_info> thermal_sensors() const override {
-      return std::vector<thermal_sensor_info>();
-    }
+        status = NvAPI_GPU_GetFullName(this->_gpu_handle, name_string);
+        if (status != NVAPI_OK) {
+          // TODO: handle error
+        }
+        return std::string{ name_string };
+      }
 
-    NvPhysicalGpuHandle _gpu_handle;
-  };
+      virtual memory_info memory() const override {
+        return memory_info();
+      }
+      virtual std::vector<thermal_sensor_info> thermal_sensors() const override {
+        return std::vector<thermal_sensor_info>();
+      }
+
+      NvPhysicalGpuHandle _gpu_handle;
+    };
+  }
 
   const std::vector<std::shared_ptr<const device>>& devices() {
     static const auto devices = std::invoke([]() -> std::vector<std::shared_ptr<const device>> {
